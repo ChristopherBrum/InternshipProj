db9617a240c287f8854f608d4fe09e7e946eba96
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-03-22 19:56:00 -0500

    troubleshooting deployment

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './Navigation.css'
-import MenuLogo from '../Images/menu-icon.jpg';
 import MenuBasket from '../Images/basket-icon.jpg';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faMenu } from '@fortawesome/free-solid-svg-icons'
 
 function Navigation() {
   // const menuIcon = <FontAwesomeIcon icon={faMenu} />
